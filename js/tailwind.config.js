11d99245af5dd9a85c105f63e203c66c93fbf3df
dumogu003 <zhangmeng.mica@gmail.com>
2025-03-28 23:11:41 +0800

    Add files via upload
    优化样式，更新css为本地文件

---
+++ js/tailwind.config.js
@@ -0,0 +1,17 @@
+// 声明全局Tailwind配置
+window.tailwindConfig = {
+  theme: {
+    extend: {
+      colors: {
+        apple: {
+          dark: '#1D1D1F',
+          gray: '#86868B',
+          blue: '#FF9500',
+          highlight: '#FFAC33',
+          black: '#000000',
+          white: '#FFFFFF',
+        }
+      }
+    }
+  }
+} 
